5c37377031efb26ab32db37ada03ad075e3f2460
Emilio Sala <emilio_k88@hotmail.com>
2023-08-17 22:18:28 +0200

    examples of types: numbers, strings...

---
+++ src/types.ts
@@ -0,0 +1,20 @@
+let name1: string = "Andrew";
+let height: number = 170;
+
+//we can't do this
+// height = "180"
+
+//in this case we don't have a type
+const GRAVITY = 9.8;
+
+//these are number
+let hexadecimal = 0xaaa;
+let binary = 0b1011;
+console.log(hexadecimal, binary); //2730 11
+
+//name is used in another file
+(() => {
+  let name = "Andrew1";
+  console.log(name1);
+
+})();
