1928334534bedf2a596d9e2fca1c2d3539f050e8
Emilio Sala <emilio_k88@hotmail.com>
2023-08-18 17:29:08 +0200

    union types, literal and alias

--- src/types.ts
+++ src/types.ts
@@ -1,6 +1,5 @@
 let name1: string = "Andrew";
 let height: number = 170;
-
 //we can't do this
 // height = "180"
 
@@ -21,3 +20,36 @@ console.log(hexadecimal, binary); //2730 11
   console.log(another)
 
 })();
+
+
+//using Object might accept things you don't want
+let array:(string | boolean| number | Object)[] = ['cat', true, 69];
+array.push(false);
+
+
+// DANGER DANGER any and cast
+let dynamic: any;
+dynamic = "WHAT AM I ? "
+console.log((dynamic as string).toLocaleLowerCase())//what am i ?
+
+//union types
+function checkID(id: string | number){
+  if (typeof id === "number"){
+    return id.toString()
+  }
+  return id;
+}
+
+console.log( checkID(332).startsWith("3")  ) //true
+
+//if we want to create an alias type
+type userID = number | string | boolean
+function createUser(user: userID ){
+  //we only need to change the type in one place
+}
+
+
+let burgerSize: "M" | "L" | "XXL"
+
+
+
