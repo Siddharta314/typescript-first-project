d9049677e05fc0187c8c03d6d35c050d5705e35b
Emilio Sala <emilio_k88@hotmail.com>
2023-09-12 20:11:27 +0200

    change gitignore

---
+++ src/app/main.ts
@@ -0,0 +1,33 @@
+import {faker} from '@faker-js/faker';
+import { addProduct, products, updateProduct, findProducts } from './products/product.service';
+
+for (let index = 0; index < 50; index++) {
+  addProduct({
+    description: faker.commerce.productDescription(),
+    image: faker.image.url(),
+    color: faker.color.human(),
+    size: faker.helpers.arrayElement(['M', 'S', 'XL', 'L']),
+    price: parseInt(faker.commerce.price(), 10),
+    isNew: faker.datatype.boolean(),
+    tags: faker.helpers.arrayElements(['tag1', 'tag2', 'tag3']),
+    title: faker.commerce.productName(),
+    stock: faker.number.int({min: 10, max: 100}),
+    categoryId: faker.database.mongodbObjectId()
+  });
+}
+
+console.log(products);
+const product = products[0];
+updateProduct(product.id, {
+  title: 'New title',
+  stock: 80,
+});
+
+
+findProducts({
+  stock: 10,
+  color: 'red',
+  createdAt: new Date(),
+  isNew: true,
+  tags: ['as', 'as']
+})
